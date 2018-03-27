9320cf40cc1299991509475024af160f7b4b760c
kaimz <k@wuwii.com>
2018-03-27 18:02:06 +0800

    add monitor

--- hystrix/consumer-hystrix/src/main/java/com/wuwii/ConsumerHystrixApplication.java
+++ hystrix/consumer-hystrix/src/main/java/com/wuwii/ConsumerHystrixApplication.java
@@ -5,12 +5,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableCircuitBreaker // 开启熔断器功能
+@EnableHystrixDashboard // Hystrix Dashboard
 public class ConsumerHystrixApplication {
 
 	public static void main(String[] args) {
